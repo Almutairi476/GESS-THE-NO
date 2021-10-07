fec8fd425762cbc09790dd84ce6a127c375343d6
Almutairi476 <you@example.com>
2021-10-07 07:50:39 -0700

    part1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+
+let randomNumber = Math.round(Math.random() * 21);
+const GuessTheNumber = () => {
+  const [value, setValue] = useState("");
+  const [results, setResults] = useState("choose a number");
+
+  const onClick = () => { 
+    if (value > randomNumber)
+      setResults(<p className="alert alert-warning">Your answer is way higher than the number</p>);
+    else if (value < randomNumber)
+      setResults(<p className="alert alert-warning">Your answer is way lower than the number</p>);
+      else{
+        setResults(<p className="alert alert-success">Nooooice!!</p>);
+      }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Guess The No.</h2>
+      {randomNumber}
+      <p className="lead">Choose a number between 1 and 20.</p>
+      <input
+        
+        type="number"
+        onChange={e => setValue(e.target.value)}
+      />
+      <button type="submit" onClick={onClick}>
+        Done
+      </button>
+      <br />
+      <br />
+      {results}
+    </>
   );
-}
+};
 
-export default App;
+export default GuessTheNumber;
